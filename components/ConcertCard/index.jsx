7c7b3a3a90ec11c33cfa8e713a246ba033cb83a3
Shikhi Tseren <shikhinee@gmail.com>
2022-05-10 22:04:31 +0800

    build error fix

--- components/ConcertCard/index.jsx
+++ components/ConcertCard/index.jsx
@@ -8,14 +8,14 @@ const ConcertCard = (props) => {
 	 return (
 		 <div className={styles.container}>
 			<div className={styles.content}>
-				<span className={styles.ariunaa}>ARIUNAA'S</span>
+				<span className={styles.ariunaa}>ARIUNAA&apos;S</span>
 				<span className={styles.special}>SPECIAL</span>
 				<span className={styles.live}>LIVE CONCERT 2022</span>
 				<span className={styles.white}>WHITE</span>
 				<span className={styles.white}>HILL</span>
 				<span className={styles.dressCode}>DRESS CODE | WHITE</span>
 				<span className={styles.location}>ZAISAN HILL COMPLEX. 3<sup>RD</sup> FLOOR</span>
-				<span className={styles.location}>Z'TERRA TERRACE AND RESTAURANT</span>
+				<span className={styles.location}>Z&apos;TERRA TERRACE AND RESTAURANT</span>
 
 			</div>
 		</div>
--- components/ConcertInfo/index.jsx
+++ components/ConcertInfo/index.jsx
@@ -8,10 +8,10 @@ const ConcertInfo = (props) => {
 	 return (
 		 <div className={styles.container}>
 			<div className={styles.content}>
-				<span className={styles.ariunaa}>ARIUNAA'S SPECIAL</span>
+				<span className={styles.ariunaa}>ARIUNAA&apos;S SPECIAL</span>
 				<span className={styles.live}>LIVE CONCERT 2022</span>
 				<span className={styles.white}>WHITE HILL</span>
-				<span className={styles.location}>FRI | 03 | JUN | AT SIX O'CLOCK</span>
+				<span className={styles.location}>FRI | 03 | JUN | AT SIX O&apos;CLOCK</span>
 				<span className={styles.menu}>DINNER MENU</span>
 				<span className={styles.food}>LOCAL BEEF STEAK WITH POTATO, GRILLED ASPARAGUS AND MUSHROOM SAUCE</span>
 				<span className={styles.food}>GREEN GARDEN SALAD | BLUEBERRY MOUSSE CAKE | GLASS OF RED WINE</span>
